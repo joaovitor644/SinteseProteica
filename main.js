b300eaf9a60b513f8b685bfab060b68356f22cd5
João Vitor <joaorasp666@yahoo.com>
2022-01-29 00:18:10 -0300

    Mudando o Código para corrigir erro do pullrequest

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
+function procesing(){
+  var saida = document.getElementById("saida")
 
-function procesing() {
-  var saida = document.getElementById("saida");
-
-  var helice = document.getElementById("dna");
-  var dna = helice.value.toUpperCase();
-  var rna = "";
-  var codon = [];
+  var helice = document.getElementById("dna")
+  var dna = helice.value.toUpperCase()
+  var rna = '';
+  var codon = []
   var proteina = [];
   var status;
-  var getCodon = "";
+  var getCodon = '';
   var codonPause = 0;
-
+  var statusOutPut;
   //contadores
   var i = 0;
   var j = 0;
@@ -20,323 +19,211 @@ function procesing() {
 
   //Aminoácidos
   var groupU = {
-    UUU: "FEN",
-    UUC: "FEN",
+      "UUU": "FEN",
+      "UUC": "FEN",
 
-    UUA: "LEU",
-    UUG: "LEU",
+      "UUA": "LEU",
+      "UUG": "LEU",
 
-    UCU: "SER",
-    UCC: "SER",
-    UCA: "SER",
-    UCG: "SER",
+      "UCU": "SER",
+      "UCC": "SER",
+      "UCA": "SER",
+      "UCG": "SER",
 
-    UAU: "TIR",
-    UAR: "TIR",
+      "UAU": "TIR",
+      "UAR": "TIR",
 
-    UGU: "CIS",
-    UGC: "CIS",
+      "UGU": "CIS",
+      "UGC": "CIS",
 
-    UGG: "TRP",
-  };
+      "UGG": "TRP"
+  }
   var groupC = {
-    CUU: "LEU",
-    CUA: "LEU",
-    CUC: "LEU",
-    CUG: "LEU",
+      "CUU": "LEU",
+      "CUA": "LEU",
+      "CUC": "LEU",
+      "CUG": "LEU",
 
-    CCU: "PRO",
-    CCC: "PRO",
-    CCA: "PRO",
-    CCG: "PRO",
+      "CCU": "PRO",
+      "CCC": "PRO",
+      "CCA": "PRO",
+      "CCG": "PRO",
 
-    CAU: "HIS",
-    CAC: "HIS",
+      "CAU": "HIS",
+      "CAC": "HIS",
 
-    CAA: "GLN",
-    CAG: "GLN",
+      "CAA": "GLN",
+      "CAG": "GLN",
 
-    CGU: "ARG",
-    CGA: "ARG",
-    CGG: "ARG",
-    CGC: "ARG",
-  };
+      "CGU": "ARG",
+      "CGA": "ARG",
+      "CGG": "ARG",
+      "CGC": "ARG"
+  }
   var groupA = {
-    AUU: "ILE",
-    AUC: "ILE",
-    AUA: "ILE",
-    AUG: "MET",
+      "AUU":"ILE",
+      "AUC":"ILE",
+      "AUA":"ILE",
+      "AUG":"MET",
 
-    ACU: "TRE",
-    ACC: "TRE",
-    ACA: "TRE",
-    ACG: "TRE",
+      "ACU":"TRE",
+      "ACC":"TRE",
+      "ACA":"TRE",
+      "ACG":"TRE",
 
-    AAU: "ASN",
-    AAC: "ASN",
+      "AAU":"ASN",
+      "AAC":"ASN",
+      
+      "AAA":"LIS",
+      "AAG":"LIS",
 
-    AAA: "LIS",
-    AAG: "LIS",
-
-    AGU: "SER",
-    AGC: "SER",
-
-    AGA: "ARG",
-    AGG: "ARG",
-  };
+      "AGU":"SER",
+      "AGC":"SER",
+      
+      "AGA":"ARG",
+      "AGG":"ARG"
+  }
   var groupG = {
-    GUU: "VAL",
-    GUA: "VAL",
-    GUC: "VAL",
-    GUG: "VAL",
+      "GUU":"VAL",
+      "GUA":"VAL",
+      "GUC":"VAL",
+      "GUG":"VAL",
 
-    GCU: "ALA",
-    GCG: "ALA",
-    GCC: "ALA",
-    GCA: "ALA",
+      "GCU":"ALA",
+      "GCG":"ALA",
+      "GCC":"ALA",
+      "GCA":"ALA",
+      
+      "GAU": "ASP",
+      "GAC": "ASP",
 
-    GAU: "ASP",
-    GAC: "ASP",
+      "GAA": "GLU",
+      "GAG": "GLU",
 
-    GAA: "GLU",
-    GAG: "GLU",
-
-    GGU: "GLI",
-    GGA: "GLI",
-    GGC: "GLI",
-    GGG: "GLI",
-  };
-  // Cria div com mensagem de erro
-  const errorMessage = document.createElement("div");
-  errorMessage.className = "divError";
-  let errorText;
-  const interface = document.querySelector(".interface");
-
-  //Funcao para recarregar a página, fazendo sumir a div
-  function locationRealod() {
-    setTimeout(function () {
-      location.reload();
-    }, 2500);
+      "GGU": "GLI",
+      "GGA": "GLI",
+      "GGC": "GLI",
+      "GGG": "GLI"
   }
 
-  while (j <= dna.length) {
-    if (dna[j] != "A" && dna[j] != "C" && dna[j] != "G" && dna[j] != "T") {
-      status = false;
-      errorText = document.createTextNode("Erro!");
-      errorMessage.appendChild(errorText);
-      interface.appendChild(errorMessage);
-
-      locationRealod();
-
-      break;
-    } else {
-      j++;
-      if (j == dna.length) {
-        status = true;
-        console.log("certo");
-        break;
+  while(j <= dna.length){
+      if(dna[j] != "A" && dna[j] != "C" && dna[j] != "G" && dna[j] != "T" ){
+          status = false;
+          window.alert("erro")
+          statusOutPut = false
+          break;
+         
       }
-function procesing(){
-    var saida = document.getElementById("saida")
-
-    var helice = document.getElementById("dna")
-    var dna = helice.value.toUpperCase()
-    var rna = '';
-    var codon = []
-    var proteina = [];
-    var status;
-    var getCodon = '';
-    var codonPause = 0;
-    var statusOutPut;
-    //contadores
-    var i = 0;
-    var j = 0;
-    var k = 0;
-    var n = 0;
-    var t = 0;
-
-    //Aminoácidos
-    var groupU = {
-        "UUU": "FEN",
-        "UUC": "FEN",
-
-        "UUA": "LEU",
-        "UUG": "LEU",
-
-        "UCU": "SER",
-        "UCC": "SER",
-        "UCA": "SER",
-        "UCG": "SER",
-
-        "UAU": "TIR",
-        "UAR": "TIR",
-
-        "UGU": "CIS",
-        "UGC": "CIS",
-
-        "UGG": "TRP"
-    }
+      else{
+          j++
+          if(j == dna.length){
+              status = true;
+              console.log("certo")
+              break;
+          } 
+      }
   }
-  if (status == true) {
-    while (dna[i]) {
-      switch (dna[i]) {
-        case "A":
-          rna += "U";
-          break;
-        case "C":
-          rna += "G";
-          break;
-        case "G":
-          rna += "C";
-          break;
-        case "T":
-          rna += "A";
-          break;
-        default:
-          break;
+  if(status == true){
+      while(dna[i]){
+          switch(dna[i]){
+              case "A":
+                  rna += "U";
+                  break;
+              case "C":
+                  rna += "G";
+                  break;
+              case "G":
+                  rna += "C";
+                  break;
+              case "T":
+                  rna += "A";
+                  break;
+              default:
+                  break;
+          }
+          i++;
       }
-      i++;
-    }
-    while (rna[k]) {
-      if (getCodon.length <= 3) {
-        getCodon += rna[k];
-        k++;
-      }
-      if (getCodon.length == 3) {
-        codon[n] = getCodon;
-        n++;
-        getCodon = "";
-      }
-    }
+          while(rna[k] ){
+          
+                  if(getCodon.length <= 3){
+                      getCodon += rna[k];
+                      k++;
+                  }
+                  if(getCodon.length == 3){
+                      codon[n] = getCodon;
+                      n++
+                      getCodon = ''
+                  }
+              
+          }
   }
-  if (rna.length % 3 == 1) {
-    var m = 0;
-    while (codon[m]) {
-      if (codon[m] == "UGA" || codon[m] == "UAG" || codon[m] == "UAA") {
-        console.log("existe códon de parada");
-        break;
-      } else {
-        m++;
+  if(rna.length % 3 == 1){
+      var m = 0;
+      while(codon[m]){
+          if(codon[m] == "UGA" || codon[m] == "UAG" || codon[m] == "UAA"){
+              console.log("existe códon de parada")
+              break;
+          } 
+          else {
+              m++
+          }
       }
-    }
+  } 
+  
+
+
+while(t < codon.length){
+  if(codon[t] == undefined){
+      break;
   }
-
-  while (t < codon.length) {
-    if (codon[t] == undefined) {
-      break;
-    while(j <= dna.length){
-        if(dna[j] != "A" && dna[j] != "C" && dna[j] != "G" && dna[j] != "T" ){
-            status = false;
-            window.alert("erro")
-            statusOutPut = false
-            break;
-           
-        }
-        else{
-            j++
-            if(j == dna.length){
-                status = true;
-                console.log("certo")
-                break;
-            } 
-        }
-    }
-    if (codon[t] == "UAA" || codon[t] == "UGA" || codon[t] == "UAG") {
-      if (t == 0) {
-        errorText = document.createTextNode("Rna inválido!");
-        errorMessage.appendChild(errorText);
-        interface.appendChild(errorMessage);
-        locationRealod();
-        //window.alert("rna inválido");
-        break;
+  if(codon[t] == "UAA" || codon[t] == "UGA" || codon[t] == "UAG"){
+      if(t == 0){
+          statusOutPut = false
+          break;
       }
       break;
-    } else if (rna.length % 3 == 1) {
-      errorText = document.createTextNode("Rna inválido!");
-      errorMessage.appendChild(errorText);
-      interface.appendChild(errorMessage);
-
-      locationRealod();
-
-      // window.alert("rna inválido");
-      break;
-    }
-    switch (codon[t][0]) {
+  } else if(rna.length % 3 == 1){
+    saida.innerHTML = "Erro no DNA , Falta duas Bases nitrogenadas para completar um códom";
+    statusOutPut = false
+    break;
+} else if(rna.length % 3 == 2){
+    saida.innerHTML = "Erro no DNA , Falta uma Bases nitrogenadas para completar um códom";
+    statusOutPut = false
+    break; 
+} else {
+      statusOutPut = true
+  }
+  switch(codon[t][0]){
       case "G":
-        proteina[t] = groupG[codon[t]];
-        t++;
-        break;
+          proteina[t] = groupG[codon[t]];
+          t++
+          break;
       case "A":
-        proteina[t] = groupA[codon[t]];
-        t++;
-        break;
+          proteina[t] = groupA[codon[t]];
+          t++
+          break;
       case "U":
-        proteina[t] = groupU[codon[t]];
-        t++;
-        break;
+          proteina[t] = groupU[codon[t]];
+          t++
+          break;
       case "C":
-        proteina[t] = groupC[codon[t]];
-        t++;
-    if(codon[t] == "UAA" || codon[t] == "UGA" || codon[t] == "UAG"){
-        if(t == 0){
-            window.alert("rna inválido");
-            statusOutPut = false
-            break;
-        }
-        break;
-    }
-    else if(rna.length % 3 == 1){
-        window.alert("rna inválido")
-        statusOutPut = false
-        break;
-    } else {
-        statusOutPut = true
-    }
-
+          proteina[t] = groupC[codon[t]];
+          t++;
+          break;
   }
-
-
-    switch(codon[t][0]){
-        case "G":
-            proteina[t] = groupG[codon[t]];
-            t++
-            break;
-        case "A":
-            proteina[t] = groupA[codon[t]];
-            t++
-            break;
-        case "U":
-            proteina[t] = groupU[codon[t]];
-            t++
-            break;
-        case "C":
-            proteina[t] = groupC[codon[t]];
-            t++;
-            break;
-    }
-    if(statusOutPut){  
-      var output =
-    "<p> DNA  => &nbsp;&nbsp;" +
-    dna +
-    "</p>" +
-    " <p>" +
-    "RNAm => " +
-    rna +
-    "</p>" +
-    "<p>" +
-    "Códon => " +
-    codon.join(" - ") +
-    "</p>" +
-    "<p>" +
-    "Aminoácidos => " +
-    proteina.join(" - ") +
-    "</p>";
-        saida.innerHTML = output ;
-    } else {
-        saida.innerHTML = "Erro, Cadeia de bases nitrogenadas inválida"
-    }
-    
+  if(statusOutPut){
+      var output = "<p> DNA  => &nbsp;&nbsp;"+dna + "</p>"+ " <p>" + "RNAm => "+rna +"</p>"+"<p>" + "Códon => "+ codon.join(' - ') + "</p>"+ "<p>" +"Aminoácidos => "+proteina.join(" - ") + "</p>"
+      saida.innerHTML = output ;
+  } else {
+      saida.innerHTML = "Erro, Cadeia de bases nitrogenadas inválida"
+  }
+  
 }
 
-
 }
+
+document.getElementById("dna").addEventListener("keypress", (obj) =>{
+  if(obj.key.toUpperCase() != "A" && obj.key.toUpperCase() != "C" && obj.key.toUpperCase() != "G" && obj.key.toUpperCase() != "T" ){
+      obj.preventDefault()
+  }
+}) 
